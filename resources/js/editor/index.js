da7f625acba0b3cdb5212f36d29e357318396891
Johnny Mast <mastjohnny@gmail.com>
2019-09-17 00:27:22 +0200

    Added 2 missing files for the project. This will be public/js/app.js and public/css/app.css.

--- resources/js/editor/index.js
+++ resources/js/editor/index.js
@@ -2,13 +2,12 @@ import { ContextMenu } from './contextmenu'
 
 const contextMenu = new ContextMenu('.editor-background')
 
-
-
-contextMenu.add('Text', () => { console.log('Text clicked')} )
+/**
+ * Create the contextmenu for the editor.
+ */
+contextMenu.add('Text', () => { console.log('Text clicked')})
   .add('Node', () => { console.log('Node clicked')})
   .add('Choice', () => { console.log('Choice clicked')})
   .add('Set', () => { console.log('Set clicked')})
   .add('Branch', () => { console.log('Branch clicked')})
   .enable()
-  // .disable()
-console.log('editor')
